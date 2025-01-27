9804de4eae5a48dd36841d1db9cf31856303088f
PSYclownYT <116015847+PSYclownYT@users.noreply.github.com>
2025-01-27 16:34:22 -0500

    whay
    whay

---
+++ Scenes/killbrick.gd
@@ -0,0 +1,9 @@
+extends CSGBox3D
+
+
+#value variables
+@export_group("value variables")
+@export var jumpBoostValue : float
+
+func _on_area_3d_area_entered(area):
+	if area.get_parent() is PlayerCharacter: area.get_parent().kill()
